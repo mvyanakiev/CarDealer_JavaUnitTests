bb0e9fff22249e1f5716f48c1794eb81f73900aa
Milko Yanakiev <myanakiev@gmail.com>
2019-03-11 13:29:09 +0200

    it working

--- src/test/java/org/softuni/cardealer/service/SaleServiceTests.java
+++ src/test/java/org/softuni/cardealer/service/SaleServiceTests.java
@@ -5,14 +5,8 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.modelmapper.ModelMapper;
-import org.softuni.cardealer.domain.models.service.CarSaleServiceModel;
-import org.softuni.cardealer.domain.models.service.CarServiceModel;
-import org.softuni.cardealer.domain.models.service.CustomerServiceModel;
-import org.softuni.cardealer.domain.models.service.PartServiceModel;
-import org.softuni.cardealer.repository.CarRepository;
-import org.softuni.cardealer.repository.CarSaleRepository;
-import org.softuni.cardealer.repository.CustomerRepository;
-import org.softuni.cardealer.repository.PartSaleRepository;
+import org.softuni.cardealer.domain.models.service.*;
+import org.softuni.cardealer.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -33,6 +27,8 @@ public class SaleServiceTests {
     private CarService carService;
     private SaleService saleService;
     private CustomerService customerService;
+    private PartService partService;
+    private SupplierService supplierService;
 
     @Autowired
     private CarRepository carRepository;
@@ -42,6 +38,11 @@ public class SaleServiceTests {
     private CarSaleRepository carSaleRepository;
     @Autowired
     private PartSaleRepository partSaleRepository;
+    @Autowired
+    private PartRepository partRepository;
+    @Autowired
+    private SupplierRepository supplierRepository;
+
     private ModelMapper modelMapper;
 
     @Before
@@ -50,39 +51,47 @@ public class SaleServiceTests {
         this.saleService = new SaleServiceImpl(this.carSaleRepository, this.partSaleRepository, this.modelMapper);
         this.carService = new CarServiceImpl(this.carRepository, this.modelMapper);
         this.customerService = new CustomerServiceImpl(this.customerRepository, this.modelMapper);
+        this.partService = new PartServiceImpl(this.partRepository, this.modelMapper);
+        this.supplierService = new SupplierServiceImpl(this.supplierRepository, this.modelMapper);
 
     }
 
     @Test
     public void saleService_saleCarWithCorrectValues_ReturnCorrect() {
+        SupplierServiceModel supplier = new SupplierServiceModel();
+        supplier.setName("Ivan");
+        supplier.setImporter(true);
+        supplier = this.supplierService.saveSupplier(supplier);
+
+        List<PartServiceModel> parts = new ArrayList<>();
+        PartServiceModel part = new PartServiceModel();
+        part.setName("door");
+        part.setPrice(BigDecimal.TEN);
+        part.setSupplier(supplier);
+        part = this.partService.savePart(part);
+        parts.add(part);
+
         CarServiceModel car = new CarServiceModel();
         car.setMake("Lada");
         car.setModel("2101");
         car.setTravelledDistance(123L);
-
-//        List<PartServiceModel> parts = new ArrayList<>();
-//        PartServiceModel part = new PartServiceModel();
-//        part.setName("door");
-//        part.setPrice(BigDecimal.TEN);
-//        parts.add(part);
-//        car.setParts(parts);
+        car.setParts(parts);
+        car = this.carService.saveCar(car);
 
         CustomerServiceModel customer = new CustomerServiceModel();
         LocalDate birthDate = LocalDate.parse("1999-08-20", DateTimeFormatter.ofPattern("yyyy-MM-dd"));
         customer.setName("George");
         customer.setBirthDate(birthDate);
         customer.setYoungDriver(true);
-        this.customerService.saveCustomer(customer);
-
-        this.carService.saveCar(car);
+        customer = this.customerService.saveCustomer(customer);
 
 
         CarSaleServiceModel carForSale = this.modelMapper.map(this.carRepository.findAll().get(0), CarSaleServiceModel.class);
         carForSale.setDiscount(10.0);
-        carForSale.setCustomer(this.modelMapper.map(this.carRepository.findAll().get(0), CustomerServiceModel.class));
+        carForSale.setCustomer(this.modelMapper.map(this.customerRepository.findAll().get(0), CustomerServiceModel.class));
 
 
-        CarSaleServiceModel actual = this.saleService.saleCar(carForSale);
+        CarSaleServiceModel actual = this.saleService.saleCar(this.modelMapper.map(carForSale, CarSaleServiceModel.class));
 
 
         CarSaleServiceModel expected = this.modelMapper.map(this.carSaleRepository.findAll().get(0), CarSaleServiceModel.class);
